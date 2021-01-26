16558af2e86424a37d23bbb30ae0d9644ac17d88
usefulparade <philae@Lukes-MacBook-Pro.local>
2021-01-26 12:31:35 -0500

    removed credentials

--- darktales/sendToSheets.js
+++
@@ -1,8 +0,0 @@
-var creds = [
-	CLIENT_ID =  '441184632000-on3lhrq01bspb28i18ptsp554flt4pdu.apps.googleusercontent.com',
-	CLIENT_SECRET = '4-vbE6JUeosfD5XYkpyQQTCH',
-	SHEET_ID = '1IWt-vM4W2SnvY5BZxDVLFXUuq82fx-PKo-x_B9-PkYY',
-
-]
-
-
